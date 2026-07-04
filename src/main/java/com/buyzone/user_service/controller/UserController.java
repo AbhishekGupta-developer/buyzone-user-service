2c6445c03ff66cc7e4b938dcd603111874dd9a20
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-07-04 20:12:31 +0530

    Add @Valid annotation

--- src/main/java/com/buyzone/user_service/controller/UserController.java
+++ src/main/java/com/buyzone/user_service/controller/UserController.java
@@ -3,6 +3,7 @@ package com.buyzone.user_service.controller;
 import com.buyzone.user_service.dto.request.UserRequestDto;
 import com.buyzone.user_service.dto.response.UserResponseDto;
 import com.buyzone.user_service.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResponseDto> registerUser(@RequestBody UserRequestDto userRequestDto) {
+    public ResponseEntity<UserResponseDto> registerUser(@Valid @RequestBody UserRequestDto userRequestDto) {
         return new ResponseEntity<>(userService.registerUser(userRequestDto), HttpStatusCode.valueOf(201));
     }
 }
